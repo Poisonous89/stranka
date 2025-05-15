f4b85984786a8886fa0cb14b4018985f42681cb7
skutas23 <skutas23@Z00P1-04>
2025-05-15 11:43:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="navigation">
+            <ul>
+                <li><div><a href="#" class="logo"><img src="images/logo.png" alt="logo"></a></div></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Join us</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Donate</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <ul>
+            <li><img src="images/logo.png" alt="logo"></li>
+            <li><p>foundation for children in need</p></li>
+            <li></li>
+        </ul>
+    </footer>
+</body>
+</html>
