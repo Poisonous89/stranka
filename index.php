b22943c1315449fc405148d61082d0b3a586ca9c
skutas23 <skutas23@Z00P1-04>
2025-06-12 11:53:11 +0200

    somestuff

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
     <header class="header">
         <nav class="navigation">
             <ul>
-                <li><div><a href="#" class="logo"><img src="images/logo.png" alt="logo"></a></div></li>
+                <li><div><a href="#" class="logo"><img class="logo" src="images/logo.png" alt="logo"></a></div></li>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About us</a></li>
                 <li><a href="#">Join us</a></li>
@@ -21,7 +21,32 @@
     </header>
     <main>
         <div class="card1">
-            <h1>Help children in Africa</h1>
+            <div class ="sub1">
+                <h1>Help children in Africa</h1>
+                <p>Every child deserves a chance to grow up healthy, educated, and safe. 
+                    Join us in providing essential support to children in Africa, 
+                    ensuring they have access to education, healthcare, and shelter.</p>
+                <div class="button"><a href="#">Learn more</a></div>
+            </div>
+            <div class="image">
+                <img class="cier" src="images/cier.png" alt="chudobne dieta">
+            </div>
+        </div>
+        <div class="section2">
+            <div class="nadpis">
+                <h1>Why to help us</h1>
+            </div>
+            <div class="d2">
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     </main>
     <footer>
@@ -34,7 +59,7 @@
                     <a href="https://www.facebook.com" alt="logo">
                         <img class="fa" src="images/fb2.png" alt="logo">
                     </a>
-                    </div>
+                </div>
             </li>
         </ul>
     </footer>
